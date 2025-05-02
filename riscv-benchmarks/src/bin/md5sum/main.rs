98fface535546a8e8fe04e1dbffe8dafff88bbfa
Connor Chang <connorc0627@gmail.com>
2025-05-02 12:28:38 -0700

    md5 stuff, simplify memory layout

--- riscv-benchmarks/src/bin/md5sum/main.rs
+++ riscv-benchmarks/src/bin/md5sum/main.rs
@@ -5,25 +5,32 @@ use riscv_benchmarks::*;
 use riscv_rt::entry;
 use md5::{Md5, Digest};
 use core::fmt::Write;
+use core::hint::black_box;
 
 const MSG_SIZE: usize = 10;
-const SCALE_FACTOR: usize = 1;
+const SCALE_FACTOR: usize = 10;
+
+fn compute_hash(data: [u8; MSG_SIZE]) -> aes::cipher::generic_array::GenericArray<u8, aes::cipher::typenum::UInt<aes::cipher::typenum::UInt<aes::cipher::typenum::UInt<aes::cipher::typenum::UInt<aes::cipher::typenum::UInt<aes::cipher::typenum::UTerm, aes::cipher::consts::B1>, aes::cipher::consts::B0>, aes::cipher::consts::B0>, aes::cipher::consts::B0>, aes::cipher::consts::B0>> {
+    let mut hasher = Md5::new();
+    hasher.update(data);
+    hasher.finalize()
+}
 
 #[entry]
 fn main() -> ! {
     let mut data: [u8; MSG_SIZE] = [0; MSG_SIZE];
-    let benchmark_data = start_benchmark();
+    for i in 0..MSG_SIZE {
+        data[i] = i as u8;
+    }
     for _ in 0..SCALE_FACTOR {
-        for i in 0..MSG_SIZE {
-            data[i] = i as u8;
-        }
-        let mut hasher = Md5::new();
-        hasher.update(data);
-        let hash = hasher.finalize();
+        let benchmark_data = start_benchmark();
+        black_box(compute_hash(black_box(data)));
+        print_benchmark_data(benchmark_data);
         // for h in hash {
         //     write!(htif::HostFile::stdout(), "{:02x?}", h).unwrap();
         // }
         // writeln!(htif::HostFile::stdout(), "").unwrap();
     }
-    verify_and_end_benchmark(&[1], &[1], benchmark_data);
+    // verify_and_end_benchmark(&[1], &[1], benchmark_data);
+    exit();
 }
